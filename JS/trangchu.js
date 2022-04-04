0bd742dee7ef7d25723302086ff4e1ae4b4b4040
TeacherLoc <102234258+TeacherLoc@users.noreply.github.com>
2022-04-04 12:56:49 +0700

    upload file

--- JS/trangchu.js
+++ JS/trangchu.js
@@ -1,3 +1,4 @@
+/* hieu ứng ảnh vào ra của thanh menu */
 const onbar = document.querySelector(".on-bar")
 onbar.addEventListener("click",function(){
 
@@ -5,9 +6,25 @@ onbar.addEventListener("click",function(){
  document.querySelector(".menu-home").classList.toggle("active")
         
 })
+/* hieu ứng parallax */
 const toP = document.querySelector(".top")
 window.addEventListener("scroll-x",function(){
     const x = this.pageXOffset;
     if(x>80){toP.classList.add("active")}
     else{toP.classList.remove("active")}
 })
+
+/* tao hiệu ứng cho bm */
+
+let anchorlinks = document.querySelectorAll('a[href^="#"]')
+for (let item of anchorlinks) { // relitere 
+    item.addEventListener('click', (e)=> {
+    let hashval = item.getAttribute('href')
+    let target = document.querySelector(hashval)
+    target.scrollIntoView({
+      behavior: 'smooth'
+    })
+    history.pushState(null, null, hashval)
+    e.preventDefault()
+  })
+}
